d70e0c7331abb8e28f95a0bfd86513547cd32463
kassl-2007 <54142001+kassl-2007@users.noreply.github.com>
2022-11-21 21:05:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,8 +29,8 @@ then
 
 	echo "Add entries to serial-starter"
 	cd ..
-	sed -i  '/service.*imt.*dbus-imt-si-rs485tc/a service epever		dbus-epever-tracer' /etc/venus/serial-starter.conf
-	sed 'ACTION=="add", ENV{ID_BUS}=="usb", ENV{ID_MODEL}=="USB_Serial",          ENV{VE_SERVICE}="epever"' /etc/udev/rules.d/serial-starter.rules
+	sed -i  '/service.*imt.*dbus-imt-si-rs485tc/a service epever		epever' /etc/venus/serial-starter.conf
+	sed '$aACTION=="add", ENV{ID_BUS}=="usb", ENV{ID_MODEL}=="USB_Serial",          ENV{VE_SERVICE}="epever"' /etc/udev/rules.d/serial-starter.rules
 
 	echo "Install driver"
 	chmod +x /data/dbus-epever-tracer/driver/start-dbus-epever-tracer.sh
